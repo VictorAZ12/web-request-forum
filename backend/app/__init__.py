3464e1e5e6742fef2698ea855bcf283e167919c8
Nikhil Sawant <165143344+23882972@users.noreply.github.com>
2024-05-18 15:52:55 +0800

    Update __init__.py
    created a new function create_app , to create a instance of the application and import blueprint from the blueprints folder

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,30 +2,44 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-from config import Config
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
+from config import Config
 import os
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-csrf = CSRFProtect(app)
-# Import models and create tables
-from app import models
-with app.app_context():
-    db_path = app.config['SQLALCHEMY_DATABASE_URI'].replace('sqlite:///', '')
-    if not os.path.exists(db_path):
-        db.create_all()
-        print("Database created successfully.")
-    else:
-        print("Database already exists.")
 
-# Create Login
-login_manager = LoginManager(app)
-# Register user_loader callback
-@login_manager.user_loader
-def load_user(user_id):
-    return models.User.query.get(int(user_id))
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+csrf = CSRFProtect()
+login_manager = LoginManager()
 
-from app import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    csrf.init_app(app)
+    login_manager.init_app(app)
+
+    from app.models import User
+
+    # Register the user_loader callback
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # Register blueprints
+    from app.blueprints.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    with app.app_context():
+        db_path = app.config['SQLALCHEMY_DATABASE_URI'].replace('sqlite:///', '')
+        if not os.path.exists(db_path):
+            db.create_all()
+            print("Database created successfully.")
+        else:
+            print("Database already exists.")
+
+    return app
