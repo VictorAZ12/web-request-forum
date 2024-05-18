48fe382e97fc2a91c2ec47d92f4e5af62f3a9e38
Nikhil Sawant <165143344+23882972@users.noreply.github.com>
2024-05-18 15:49:59 +0800

    Update config.py added TestingConfig

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,15 @@
 import os
+
 # Config file
 # Source: https://flask.palletsprojects.com/en/2.3.x/config/#configuring-from-python-files
 
 class Config(object):
     """Maintains flask app configs"""
-    SECRET_KEY = '12345678' # Test purpose only
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'webapp.db')
+    SECRET_KEY = '12345678'  # Test purpose only
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'webapp.db')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    WTF_CSRF_ENABLED = False
